632a19a192a7876f3ab1b8cf73c0b1609ab6130e
Bishal Sunuwar <bishalsunuwar24@gmail.com>
2024-01-12 20:22:57 +0545

    detail page

---
+++ src/components/Jobs/JobsDetail.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { jobActions } from "../../store/job-slice";
+
+const JobsDetail = ({ id, imgUrl, name, position }) => {
+  const dispatch = useDispatch();
+  const addToSaveJobs = () => {
+    dispatch(
+      jobActions.addJobToCart({
+        id,
+        name,
+        position,
+      })
+    );
+  };
+
+  return (
+    <>
+      <div className="flex flex-col items-center">
+        <div className="border-2">
+          <img src={imgUrl} alt={name} />
+          <h3>{name}</h3>
+          <h1>{position}</h1>
+        </div>
+        <div className="border-2 h-auto w-4/12 m-4">
+          <h1>Job Description</h1>
+          <div>
+            <ul className="list-disc">
+              <li className="">
+                <h1 className="font-bold">
+                  Loan Marketing and Identifying the Potential Customers’:{" "}
+                </h1>
+                <p>
+                  Market the products of the organization for increasing the
+                  loan customers of the organization for maximizing the profit
+                  of the organization.
+                </p>
+              </li>
+              <li>
+                <h1 className="font-bold">
+                  Pre-Proposal Writing and Pre- Report Preparation:
+                </h1>
+                <p>
+                  Prepare credit appraisal report by obtaining and finding the
+                  real and genuine information of the customers about the credit
+                  history of the client.
+                </p>
+              </li>
+            </ul>
+          </div>
+          <hr />
+          <div className="">
+            <div className="m-4 flex justify-center">
+              <button className="bg-cyan-200 rounded p-2">Apply Now</button>
+            </div>
+            <hr />
+            <div className="m-4 flex justify-center">
+              <button
+                className="bg-cyan-200 rounded p-2"
+                onClick={addToSaveJobs}
+              >
+                Save Job
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default JobsDetail;
