70e03728fabd662f9f827a6253440e28e7dc2d5a
BishalSunuwar202 <bishalsunuwar24@gmail.com>
2024-02-13 18:09:52 +0545

    signup controller logic

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,14 +1,17 @@
 import bcrypt from "bcrypt";
 
-
 import { User } from "../models/user.model.js";
 
 export const signUp = async (req, res, next) => {
   try {
-    const hash = bcrypt.hashSync(req.body.password, 10)
-    const signing = await User.create({...req.body, password: hash});
-    console.log(signing)
-    return res.status(201).json(signing);
+    const { password, ...userData } = req.body;
+    const hash = bcrypt.hashSync(password, 10);
+
+    const signing = await User.create({ ...userData, password: hash });
+    const signinObject = signing.toObject()
+    delete signinObject.password;
+
+    return res.status(201).json(signinObject);
   } catch (err) {
     next(err);
   }
