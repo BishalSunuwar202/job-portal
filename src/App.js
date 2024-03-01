a273f5b5352e2b733e7c4d9b5539a93dee500147
BishalSunuwar202 <bishalsunuwar24@gmail.com>
2024-03-01 21:41:09 +0545

    route and useeffect logic

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import "./App.css";
 import RootLayout from "./pages/Root";
 import HomePage from "./pages/Home";
 import ErrorPage from "./pages/ErrorPage";
-import AuthenticationPage from "./pages/Authentication";
-import LoginPage from "./pages/LoginPage";
+import AuthenticationPage, {
+  action as signupAction,
+} from "./pages/Authentication";
+import LoginPage, { action as loginAction } from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
-import ProfileRootLayout from "./pages/ProfileRootLayout";
+//import ProfileRootLayout from "./pages/ProfileRootLayout";
 import CompanyDetailPage from "./pages/CompanyDetailPage";
 import SavedJobsPage from "./pages/SavedJobsPage";
 import ProfileFormPage, {
   action as newProfileAction,
   loader as profileDetailLoader,
 } from "./pages/ProfileFormPage";
+
 import SearchPage from "./pages/Search";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { userAction } from "./store/user-slice";
 
 const router = createBrowserRouter([
   {
@@ -28,10 +34,12 @@ const router = createBrowserRouter([
       {
         path: "auth/signup",
         element: <AuthenticationPage />,
+        action: signupAction,
       },
       {
         path: "auth/login",
         element: <LoginPage />,
+        action: loginAction,
       },
       {
         path: "employer/:id",
@@ -42,9 +50,11 @@ const router = createBrowserRouter([
         element: <SavedJobsPage />,
       },
       { path: "search", element: <SearchPage /> },
+
+      //{element: <PrivateRoute/> },
       {
         path: "profile",
-        element: <ProfileRootLayout />,
+        // element: <ProfileRootLayout />,
         children: [
           {
             index: true,
@@ -63,7 +73,50 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (localStorage.getItem("accessToken")) {
+      fetch("http://localhost:3001/api/userList", {
+        credentials: "include",
+      })
+        .then(async (res) => {
+         // console.log(await res.json());
+          dispatch(userAction.login());
+          //dispatch(userAction.setUser(JSON.stringify(res)));
+          dispatch(userAction.setUser(await res.json()));
+          setLoading(true);
+        })
+        .catch((error) => {
+          console.error(error)
+          dispatch(userAction.logout());
+          setLoading(true);
+        });
+    } else {
+      setLoading(true);
+    }
+    
+  }, []);
+  if (!loading) {
+    return (
+      <>
+        <div className="flex justify-center items-center h-screen">
+          <button type="button" className="bg-indigo-500 ..." disabled>
+            <svg
+              className="animate-spin h-5 w-5 mr-3 ..."
+              viewBox="0 0 24 24"
+            ></svg>
+            Loading...
+          </button>
+        </div>
+      </>
+    );
+  }
+
   return <RouterProvider router={router} />;
 }
 
 export default App;
+
+//.json() is used to extract and parse the JSON payload from the response body.
+//JSON.stringify() is a method in JavaScript used to convert JavaScript objects into JSON strings.
